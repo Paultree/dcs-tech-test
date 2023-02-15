d93d1c3fe99cac3b536d2e18d761f562ed7e5f14
paul pham <paulpham3000@gmail.com>
2023-02-15 19:01:09 +0900

    added styling to employee form

--- front-end/src/components/EmployeeForm/EmployeeForm.tsx
+++ front-end/src/components/EmployeeForm/EmployeeForm.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { useForm, SubmitHandler } from "react-hook-form";
+import styles from "./EmployeeForm.module.scss";
 
 enum ContractTypeEnum {
   permanent = "permanent",
@@ -31,40 +32,63 @@ export const EmployeeForm = () => {
   const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>First Name</label>
-      <input {...register("firstName")} />
-      <label>Middle Name</label>
-      <input {...register("middleName")} />
-      <label>Last Name</label>
-      <input {...register("lastName")} />
-      <label>Email Address</label>
-      <input {...register("email")} />
-      <label>Mobile Number</label>
-      <input {...register("mobileNumber")} />
-      <label>Residential Address</label>
-      <input {...register("address")} />
-      <label>Contract Type</label>
-      <div {...register("contractType")}>
-        <input type="radio" value="permanent" />
-        Permanent
-        <input type="radio" value="contract" />
-        Contract
+    <form className={styles.EmployeeForm} onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles.EmployeeForm_PersonalDetails}>
+        <h3>Personal Information</h3>
+        <label>First Name</label>
+        <input {...register("firstName")} />
+        <label>Middle Name (if applicable)</label>
+        <input {...register("middleName")} />
+        <label>Last Name</label>
+        <input {...register("lastName")} />
       </div>
-      <label htmlFor="">Start Date</label>
-      <input type="date" />
-      <label htmlFor="">End Date</label>
-      <input type="date" />
-      <label>Is this on a full-time or part-time basis?</label>
-      <div {...register("employTime")}>
-        <input type="radio" value="full time" />
-        Full Time
-        <input type="radio" value="part time" />
-        Part Time
+      <div className={styles.EmployeeForm_ContactDetails}>
+        <h3>Contact Details</h3>
+        <label>Email Address</label>
+        <input {...register("email")} />
+        <label>Mobile Number</label>
+        <input {...register("mobileNumber")} />
+        <label>Residential Address</label>
+        <input
+          className={styles.EmployeeForm_ContactDetails_Address}
+          {...register("address")}
+        />
       </div>
-      <label>Hours Per Week</label>
-      <input {...register("hoursPerWk")} />
-      <input type="submit" />
+      <div className={styles.EmployeeForm_EmployeeStatus}>
+        <h3>Employee Status</h3>
+        <label>Contract Type</label>
+        <div {...register("contractType")}>
+          <div>
+            <input type="radio" value="permanent" />
+            Permanent
+          </div>
+          <div>
+            <input type="radio" value="contract" />
+            Contract
+          </div>
+        </div>
+        <label htmlFor="">Start Date</label>
+        <input type="date" />
+        <label htmlFor="">End Date</label>
+        <input type="date" />
+        <label>Is this on a full-time or part-time basis?</label>
+        <div {...register("employTime")}>
+          <div>
+            <input type="radio" value="full time" />
+            Full Time
+          </div>
+          <div>
+            <input type="radio" value="part time" />
+            Part Time
+          </div>
+        </div>
+        <label>Hours Per Week</label>
+        <input
+          className={styles.EmployeeForm_EmployeeStatus_Hours}
+          {...register("hoursPerWk")}
+        />
+        <input type="submit" />
+      </div>
     </form>
   );
 };
--- front-end/src/containers/NewEmployeePage/NewEmployeePage.tsx
+++ front-end/src/containers/NewEmployeePage/NewEmployeePage.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { EmployeeForm } from "../../components/EmployeeForm/EmployeeForm";
+import styles from "./NewEmployeePage.module.scss";
 
 const NewEmployeePage = () => {
   const navigate: void = useNavigate();
 
   return (
-    <div>
+    <div className={styles.NewEmployeePage}>
       <h1>Employee Details</h1>
       <EmployeeForm />
     </div>
