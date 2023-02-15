af0a725e5e757d389be93b551b35187fbe11de7c
paul pham <paulpham3000@gmail.com>
2023-02-15 19:49:21 +0900

    basic validations added to form

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # dcs-tech-test
 
 Employee information app built with React + Springboot
+
+- use input type date - deviating from the MVP
+- remove ongoing checkbox
+  - end date should be grayed out if full time is picked.
+
+first and last name regex pattern
+https://stackoverflow.com/questions/2385701/regular-expression-for-first-and-last-name
+
+phone number
+https://stackoverflow.com/questions/22378736/regex-for-mobile-number-validation#:~:text=%2F%5E(%5B%2B%5D%5C,%5Cd%7B10%7D%24%2F&text=This%20is%20how%20this%20regex,world%20wide%20matching%20of%20number.
+
+email
+https://www.w3resource.com/javascript/form/email-validation.php#:~:text=To%20get%20a%20valid%20email,%5D%2B)*%24%2F.
--- front-end/src/components/EmployeeForm/EmployeeForm.tsx
+++ front-end/src/components/EmployeeForm/EmployeeForm.tsx
@@ -21,8 +21,8 @@ interface IFormInput {
   mobileNumber: number;
   address: String;
   contractType: ContractTypeEnum;
-  startDate: LocalDate;
-  endDate: LocalDate | null;
+  startDate: String;
+  endDate: String | null;
   employTime: TimeEnum;
   hoursPerWk: number;
 }
@@ -36,34 +36,71 @@ export const EmployeeForm = () => {
       <div className={styles.EmployeeForm_PersonalDetails}>
         <h3>Personal Information</h3>
         <label>First Name</label>
-        <input {...register("firstName")} />
+        <input
+          {...register("firstName", {
+            required: true,
+            pattern: /^[a-z ,.'-]+$/i,
+          })}
+        />
         <label>Middle Name (if applicable)</label>
-        <input {...register("middleName")} />
+        <input
+          {...register("middleName", {
+            pattern: /^[a-z ,.'-]+$/i,
+          })}
+        />
         <label>Last Name</label>
-        <input {...register("lastName")} />
+        <input
+          {...register("lastName", {
+            required: true,
+            pattern: /^[a-z ,.'-]+$/i,
+          })}
+        />
       </div>
       <div className={styles.EmployeeForm_ContactDetails}>
         <h3>Contact Details</h3>
         <label>Email Address</label>
-        <input {...register("email")} />
+        <input
+          {...register("email", {
+            required: true,
+            pattern:
+              /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+          })}
+        />
         <label>Mobile Number</label>
-        <input {...register("mobileNumber")} />
+        <input
+          type="tel"
+          {...register("mobileNumber", {
+            required: true,
+            pattern: /^\d{10}$/,
+          })}
+        />
         <label>Residential Address</label>
         <input
           className={styles.EmployeeForm_ContactDetails_Address}
-          {...register("address")}
+          {...register("address", {
+            required: true,
+            pattern: /\w+(\s\w+){2,}/,
+          })}
         />
       </div>
       <div className={styles.EmployeeForm_EmployeeStatus}>
         <h3>Employee Status</h3>
         <label>Contract Type</label>
-        <div {...register("contractType")}>
+        <div>
           <div>
-            <input type="radio" value="permanent" />
+            <input
+              type="radio"
+              value="permanent"
+              {...register("contractType", { required: true })}
+            />
             Permanent
           </div>
           <div>
-            <input type="radio" value="contract" />
+            <input
+              type="radio"
+              value="contract"
+              {...register("contractType", { required: true })}
+            />
             Contract
           </div>
         </div>
@@ -72,20 +109,28 @@ export const EmployeeForm = () => {
         <label htmlFor="">End Date</label>
         <input type="date" />
         <label>Is this on a full-time or part-time basis?</label>
-        <div {...register("employTime")}>
+        <div>
           <div>
-            <input type="radio" value="full time" />
+            <input
+              type="radio"
+              value="fullTime"
+              {...register("employTime", { required: true })}
+            />
             Full Time
           </div>
           <div>
-            <input type="radio" value="part time" />
+            <input
+              type="radio"
+              value="partTime"
+              {...register("employTime", { required: true })}
+            />
             Part Time
           </div>
         </div>
         <label>Hours Per Week</label>
         <input
           className={styles.EmployeeForm_EmployeeStatus_Hours}
-          {...register("hoursPerWk")}
+          {...register("hoursPerWk", { required: true, min: 3, max: 38 })}
         />
         <input type="submit" />
       </div>
